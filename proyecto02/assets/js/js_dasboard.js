5f286ac775a4bdbf32c83869986655aafe857318
M. Alexandra Ramos <maraplua@espol.edu.ec>
2022-11-26 21:38:46 -0500

    dashboard

--- proyecto02/assets/js/js_dasboard.js
+++ proyecto02/assets/js/js_dasboard.js
@@ -1,20 +1,100 @@
-.then(response => response.json())
-  .then(data => {
-   
-     /*=== categorias de producto ===*/   
-    let categorias =[];  
-    let categ= [];  
-    for (let i of data) {
-      categorias.push(i.category)
-     
-    }   
-    categorias.forEach( (elemento) => {
-      if (!categ.includes(elemento)) {
-        categ.push(elemento);
-      }
-    });
-   
-    
-  })
-  .catch(console.error);
+window.addEventListener('DOMContentLoaded', (event) => {
+  cargarDatos()
+  Filtrar()
 
+});
+
+let cargarDatos = () => {
+  fetch("https://fakestoreapi.com/products")
+    .then(response => response.json())
+    .then(data => {
+      /*=== categorias de producto ===*/
+      let categorias = [];
+      let categ = [];
+      for (let i of data) {
+        categorias.push(i.category)
+
+      }
+      categorias.forEach((elemento) => {
+        if (!categ.includes(elemento)) {
+          categ.push(elemento);
+          console.log(elemento)
+        }
+      });
+
+
+      for (let i of categ) {
+        let plan = `
+          <option id="${i}">${i}</options>
+          `
+        document.getElementById("opciones").innerHTML += plan
+      }
+
+
+
+    })
+    .catch(console.error);
+}
+
+let Filtrar = () => {
+
+  /*=== categorias de producto ===*/
+  let select = document.getElementById("opciones")
+  select.addEventListener("change", (event) => {
+    let valor = event.target.value;
+    fetch("https://fakestoreapi.com/products")
+      .then(response => response.json())
+      .then(data => {
+        let articulo_categ = data.filter(data => data.category == valor)
+        document.getElementById("chart").innerHTML = ''
+        // grafica 1
+        for (let i of articulo_categ) {
+          let plantilla =`
+            <tr>
+              <th scope="row"> ${i.title.slice(0,10)} </th>
+              <td style="--size:calc(${i.price }/200)"> $${i.price} </td>     
+            </tr>                              
+
+          `
+          document.getElementById("chart").innerHTML += plantilla
+        }
+      // grafica 2
+        document.getElementById("tchart2").innerHTML = ''
+        
+        for (let i of articulo_categ) {
+          let plantilla2 =`
+            <tr>
+              <th scope="row"> ${i.title.slice(0,10)} </th>
+              <td style="--size:calc(${i.rating.rate})"> ${i.rating.rate} </td>     
+            </tr>           
+           
+
+          `
+          document.getElementById("tchart2").innerHTML += plantilla2
+        }
+
+        document.getElementById("filtro").innerHTML = ''
+        
+        for (let i of articulo_categ) {
+          let plantilla3 =`
+          <tr>
+            <th scope="row"> ${i.title.slice(0,10)} </th>
+            <td> $${i.price }</td>
+            <td> ${i.rating.rate} </td>           
+          </tr>                   
+
+          `
+          document.getElementById("filtro").innerHTML += plantilla3
+        }
+
+
+
+      })
+      .catch(console.error);
+
+
+
+
+  })
+
+}
